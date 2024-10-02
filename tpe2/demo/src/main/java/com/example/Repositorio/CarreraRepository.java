2994197f3ad2fda30b32adeb23a3e76973100db6
braianricci <braianricci@gmail.com>
2024-10-01 22:05:54 -0300

    fixes generales

--- tpe2/demo/src/main/java/com/example/Repositorio/CarreraRepository.java
+++ tpe2/demo/src/main/java/com/example/Repositorio/CarreraRepository.java
@@ -3,8 +3,11 @@ package com.example.Repositorio;
 import java.util.List;
 
 import com.example.DTO.CarreraDTO;
+import com.example.Entidad.Carrera;
 
 public interface CarreraRepository {
+
+    public void altaCarrera(Carrera carrera);
 
     public List<CarreraDTO> recuperarCarrerasConInscriptos();
 
